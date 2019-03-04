a2d3b494e48420979eed9f754e60f4acdf490263
risheric <risheric@gmail.com>
2019-03-04 15:43:21 -0300

    first iteration of the stew generator

--- main.py
+++ main.py
@@ -2,15 +2,60 @@ import string
 import numpy as np
 import json
 
-fild = np.random.choice(list(string.ascii_lowercase),  size=(15, 15))
+FILD_X = 15
+FILD_Y = 15
 
 
-with open('palabras.json', 'r') as f:
-    words = json.load(f)
+fild = np.zeros([FILD_Y,FILD_X], dtype='str')
 
 
-fild = np.random.choice(list(string.ascii_lowercase),  size=(15, 15))
+def insert_vertical(fild, word='codin',backwards=False):
+    if(backwards):
+        word = word[::-1]
+    flag = False
+    count = 0
+    while(flag == False):
+        count+=1
+        #generate random x,y
+        pos = (np.random.randint(FILD_Y),np.random.randint(FILD_X-len(word)))
+        #get that space and check if there is no letter there
+        fild_row = fild[pos[0]]
+        space = fild_row[pos[1]:pos[1]+len(word)]
+        space = ''.join(space)
+        if(len(space) == 0):
+            for i in range(len(word)):
+                fild_row[i+pos[1]] = word[i]
+            flag = True
+        assert count < 10, "something went terrible wrong!"
 
-# insert word in fild:
-# generate random pos to insert
-# check that the word is not going out of bound
+
+def insert_horizontal(fild, word,backwards=False):
+    if(backwards):
+        word = word[::-1]
+    flag = False
+    count = 0
+    while(flag == False):
+        count+=1
+        #generate random x,y
+        pos = (np.random.randint(FILD_Y-len(word)),np.random.randint(FILD_X))
+        #get that space and check if there is no letter there
+        space = fild[pos[0]:pos[0]+len(word),pos[1]]
+        space = ''.join(space)
+        if(len(space) == 0):
+            for i in range(len(word)):
+                fild[pos[0]+i,pos[1]] = word[i]
+            flag = True
+        assert count < 10, "something went terrible wrong!"
+
+
+def fill_with_random(fild):
+    for x in range(FILD_X):
+        for y in range(FILD_Y):
+            if(fild[x,y] == ''):
+                fild[x,y] = np.random.choice(list(string.ascii_lowercase))
+
+insert_horizontal(fild, 'Codin',True)
+insert_vertical(fild, 'Eric')
+fill_with_random(fild)
+print(fild)
+
